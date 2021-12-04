e7242da9441f3838d1f8991d22fc21a8c78f2f34
saundersp <pierre.saundersgb@gmail.com>
2021-12-04 03:36:43 +0100

    installer : fixed config propagation

--- installer/install.sh
+++ installer/install.sh
@@ -5,7 +5,8 @@ bash preinstall.sh
 useradd $USERNAME
 mkdir -p /mnt/home/$USERNAME/git/
 chown -R $USERNAME:$USERNAME dotfiles
-cp -r dotfiles /mnt/home/$USERNAME/git
+cp -r /root/dotfiles /mnt/home/$USERNAME/git
+cp /root/dotfiles/installer/config.conf /mnt/root
 arch-chroot /mnt /home/$USERNAME/git/dotfiles/installer/root_install.sh
 arch-chroot /mnt /usr/bin/runuser -u $USERNAME /home/$USERNAME/git/dotfiles/installer/user_install.sh
 
