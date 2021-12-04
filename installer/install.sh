9b2b76444a130218e475cba99764e38ba463c835
saundersp <pierre.saundersgb@gmail.com>
2021-12-04 02:51:24 +0100

    installer : changed script paths

--- installer/install.sh
+++ installer/install.sh
@@ -3,6 +3,6 @@
 bash preinstall.sh
 mkdir -p /mnt/home/_aspil0w/git/
 cp -r /root/dotfiles /mnt/home/_aspil0w/git
-arch-chroot /mnt /root/root_install.sh
-arch-chroot /mnt /usr/bin/runuser -u _aspil0w /home/_aspil0w/user_install.sh
+arch-chroot /mnt /home/_aspil0w/git/dotfiles/installer/root_install.sh
+arch-chroot /mnt /usr/bin/runuser -u _aspil0w /home/_aspil0w/git/dotfiles/installer/user_install.sh
 
