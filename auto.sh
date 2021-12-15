63ec1227fff59b70671ed534fdc99204a25eaf2a
saundersp <pierre.saundersgb@gmail.com>
2021-12-15 01:28:18 +0100

    auto.sh : fixed wrong root bash_profile && changed " to '

--- auto.sh
+++ auto.sh
@@ -6,7 +6,7 @@ export XDG_DATA_HOME=$HOME/.XDG/data
 mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME
 
 CURRENT_FOLDER=$(pwd)
-if [[ $1 == "uninstall" || $1 == "remove" ]]; then
+if [[ $1 == 'uninstall' || $1 == 'remove' ]]; then
 	rm -rf $XDG_CONFIG_HOME/nvim/init.lua $XDG_CONFIG_HOME/neofetch/config.conf $HOME/.bashrc $HOME/.gitconfig $HOME/.bash_profile \
 		$XDG_CONFIG_HOME/alacritty/alacritty.yml $XDG_CONFIG_HOME/i3/config $HOME/.xinitrc $XDG_CONFIG_HOME/nvim/coc-settings.json \
 		$XDG_CONFIG_HOME/polybar/launch.sh $XDG_CONFIG_HOME/polybar/config $XDG_CONFIG_HOME/polybar/scripts
@@ -24,9 +24,9 @@ elif [ $EUID -ne 0 ]; then
 	ln -s $CURRENT_FOLDER/.bashrc $HOME/.bashrc
 	ln -s $CURRENT_FOLDER/.gitconfig $HOME/.gitconfig
 
-	if [[ $1 == "server" ]]; then
+	if [[ $1 == 'server' ]]; then
 		ln -s $CURRENT_FOLDER/.bash_profile_server $HOME/.bash_profile
-	elif [[ $1 == "windows" ]]; then
+	elif [[ $1 == 'windows' ]]; then
 		rm -f $XDG_CONFIG_HOME/alacritty/alacritty.yml $XDG_CONFIG_HOME/nvim/init.lua $XDG_CONFIG_HOME/neofetch/config.conf $HOME/.bashrc $HOME/.gitconfig \
 			$XDG_CONFIG_HOME/nvim/coc-settings.json
 
@@ -74,5 +74,5 @@ else
 
 	ln -s $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
 	ln -s $CURRENT_FOLDER/.bashrc $HOME/.bashrc
-	ln -s $CURRENT_FOLDER/root_bash_profile $HOME/.bash_profile
+	ln -s $CURRENT_FOLDER/root.bash_profile $HOME/.bash_profile
 fi
