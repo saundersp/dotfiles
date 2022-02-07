1917e311c0e4f2b3ac9bdf23d7391264bba893c0
saundersp <pierre.saundersgb@gmail.com>
2022-02-07 22:40:22 +0100

    installer : gentoo : fixed kernel compilation ^&^& added more packages

--- installer/gentoo.sh
+++ installer/gentoo.sh
@@ -12,7 +12,6 @@ DISK_LAST_SECTOR=
 BOOT_PARTITION_INDEX=1
 ROOT_PARTITION_INDEX=2
 PARTITION_SEPARATOR=
-FONT_PATH=/usr/share/fonts
 PACKAGES=virtual
 # Other options : virtual laptop server minimal
 SWAP_SIZE=4G
@@ -121,14 +120,13 @@ eselect profile set 1
 # Apply the default USE flags to the @world set
 emerge --quiet --update --deep --newuse @world
 
-# Configuring the USE flags
-# Get all enabled flags : portageq envvar USE | xargs -n1
-# Note : use 'less /var/db/repos/gentoo/profiles/use.desc' to get a description of each variables
-
 # Removing the existing package.use
 rm -rf /etc/portage/package.use /etc/portage/package.accept_keywords /etc/portage/package.mask
 touch /etc/portage/package.use /etc/portage/package.accept_keywords /etc/portage/package.mask
 
+# Configuring the USE flags
+# Get all enabled flags : portageq envvar USE | xargs -n1
+# Note : use 'less /var/db/repos/gentoo/profiles/use.desc' to get a description of each variables
 case $PACKAGES in
 	minimal)
 		# Global flags
@@ -144,7 +142,7 @@ case $PACKAGES in
 		# Global flags
 		echo 'USE=\"X bash-completion elogind python\"' >> /etc/portage/make.conf
 		# Local flags
-		echo 'x11-misc/polybar i3wm' > /etc/portage/package.use
+		echo -e 'x11-misc/polybar i3wm\n>=sys-libs/zlib-1.2.11-r4 minizip\nmedia-libs/libvpx postproc' > /etc/portage/package.use
 		# Package accept_keywords
 		echo 'app-editors/neovim ~amd64' >> /etc/portage/package.accept_keywords
 	;;
@@ -152,11 +150,10 @@ case $PACKAGES in
 		# Global flags
 		echo 'USE=\"X bash-completion connman elogind pulseaudio python\"' >> /etc/portage/make.conf
 		# Local flags
-		echo 'x11-misc/polybar i3wm' > /etc/portage/package.use
+		echo -e 'x11-misc/polybar i3wm\n>=sys-libs/zlib-1.2.11-r4 minizip\nmedia-libs/libvpx postproc' > /etc/portage/package.use
 		# Package accept_keywords
 		echo 'app-editors/neovim ~amd64' >> /etc/portage/package.accept_keywords
 	;;
-	*) exit 1 ;;
 esac
 
 # Setting the timezone
@@ -175,8 +172,8 @@ emerge --quiet sys-fs/cryptsetup
 rc-update add dmcrypt boot
 
 # Compilation and installation of the kernel
-#emerge --quiet sys-kernel/gentoo-sources
-#eselect kernel set 1
+emerge --quiet sys-kernel/gentoo-sources
+eselect kernel set 1
 #cd /usr/src/linux
 #make menuconfig
 #make
@@ -223,7 +220,7 @@ EOF
 
 # Installing and setting up doas
 emerge --quiet app-admin/doas
-echo 'permit nopass :wheel' > /etc/doas.conf
+echo -e 'permit nopass :wheel\npermit nopass :wheel cmd poweroff\npermit nopass :wheel cmd reboot' > /etc/doas.conf
 
 # Replace sudo
 ln -s /usr/bin/doas /usr/bin/sudo
@@ -243,7 +240,7 @@ echo 'GRUB_PLATFORMS=\"efi-64\"' >> /etc/portage/make.conf
 emerge --quiet sys-boot/grub
 
 # Prepare boot loader for LUKS
-sed -i \"s,^#GRUB_CMDLINE_LINUX=\\\"\\\",GRUB_CMDLINE_LINUX=\\\"crypt_root=UUID=\$(blkid -s UUID -o value $ROOT_PARTITION) root=/dev/mapper/root keymap=$KEYMAP\\\",g\" /etc/default/grub
+sed -i \"s,^#GRUB_CMDLINE_LINUX=\\\"\\\",GRUB_CMDLINE_LINUX=\\\"quiet crypt_root=UUID=\$(blkid -s UUID -o value $ROOT_PARTITION) root=/dev/mapper/root keymap=$KEYMAP\\\",g\" /etc/default/grub
 
 # Enable os-prober if laptop
 if [ '$PACKAGES' == 'laptop' ]; then
@@ -263,100 +260,86 @@ emerge --quiet sys-auth/elogind sys-apps/which
 
 install_server(){
 	emerge --quiet app-misc/neofetch app-editors/neovim sys-apps/which dev-vcs/git net-misc/wget app-arch/unzip app-shells/bash-completion net-libs/nodejs \
-					dev-lang/python sys-apps/ripgrep sys-apps/man-db dev-python/pip
+					dev-lang/python sys-apps/ripgrep sys-apps/man-db dev-python/pip sys-process/htop dev-lang/go
+
+	# Creating user's custom packages location
+	mkdir /usr/local/src
+
+	# Compiling lazygit
+	cd /usr/local/src
+	git clone https://github.com/jesseduffield/lazygit.git
+	cd lazygit
+	sudo -u $USERNAME go install
+	mv /home/$USERNAME/go/bin/lazygit /usr/bin/lazygit
+	cd \$HOME
+
+	# Installing npm dependencies
+	npm i -g neovim npm-check-updates
+
+	# Installing pip dependencies
+	sudo -u $USERNAME pip install --user pynvim autopep8 flake8
+	ls /home/$USERNAME/.local/bin | xargs -I{} sudo ln -s /home/$USERNAME/.local/bin/{} /usr/bin/{}
+
 }
 install_ihm(){
+	install_server
 	emerge --quiet x11-misc/dmenu x11-misc/picom x11-wm/i3-gaps x11-apps/xinit x11-base/xorg-server x11-apps/xset media-gfx/feh x11-terms/alacritty \
-					x11-misc/polybar x11-apps/xrandr x11-misc/xclip x11-apps/setxkbmap
+					x11-misc/polybar x11-apps/xrandr x11-misc/xclip x11-apps/setxkbmap www-client/firefox
+
+	# Getting the Hasklig font
+	wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hasklig.zip
+	mkdir /usr/share/fonts/Hasklig
+	unzip -q Hasklig.zip -d /usr/share/fonts/Hasklig
+	rm Hasklig.zip
 }
+install_dotfiles(){
+	# Getting the dotfiles
+	mkdir ~/git
+	git clone https://github.com/saundersp/dotfiles.git ~/git/dotfiles
+
+	# Enabling the dotfiles
+	cd ~/git/dotfiles
+	./auto.sh \$@
+	sudo bash auto.sh \$@
+
+	# Getting the wallpaper
+	mkdir ~/Images
+	cd ~/Images
+	wget -q --show-progress https://www.pixelstalk.net/wp-content/uploads/2016/07/HD-Astronaut-Wallpaper.jpg
+}
+export -f install_dotfiles
 
 case $PACKAGES in
 	virtual)
 		install_ihm
-		#emerge --quiet app-emulation/virtualbox-guest-additions
-	;&
-	server) install_server ;&
+		emerge --quiet app-emulation/virtualbox-guest-additions
+		su $USERNAME -c \"install_dotfiles $PACKAGES\"
+	;;
+	server)
+		install_server
+		su $USERNAME -c \"install_dotfiles $PACKAGES\"
+		;;
 	minimal) ;;
 	laptop)
-		install_minimal
-		install_server
 		install_ihm
+		su $USERNAME -c \"install_dotfiles $PACKAGES\"
+		echo 'permit nopass :wheel cmd brightnessctl' >> /etc/doas.conf
 		emerge --quiet net-wireless/iw net-wireless/wpa_supplicant
 	;;
-	*) exit 1 ;;
 esac
 
 # Start elogind service at boot
 rc-update add elogind boot
 
-if [ '$PACKAGES' != 'minimal' ]; then
-	# Installing npm dependencies
-	npm i -g neovim npm-check-updates
-
-	# Installing pip dependencies
-	pip install --user pynvim autopep8 flake8
-	# TODO : Fix install as root && softlink to /usr/bin
-fi
-
-if [[ '$PACKAGES' == 'laptop' || '$PACKAGES' == 'virtual' ]]; then
-	# Getting the Hasklig font
-	wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hasklig.zip
-	mkdir $FONT_PATH/Hasklig
-	unzip -q Hasklig.zip -d $FONT_PATH/Hasklig
-	rm Hasklig.zip
-fi
+# Removing the nopass option in doas
+sed -i '1s/nopass/persist/g' /etc/doas.conf
 
 " > /mnt/gentoo/install.sh
 chmod +x /mnt/gentoo/install.sh
 chroot /mnt/gentoo /bin/bash /install.sh
 
-echo "#!/usr/bin/env bash
-
-# Exit immediately if a command exits with a non-zero exit status
-set -e
-
-if [ '$PACKAGES' != 'minimal' ]; then
-	# Getting the dotfiles
-	mkdir /home/$USERNAME/git
-	git clone https://github.com/saundersp/dotfiles.git /home/$USERNAME/git/dotfiles
-fi
-
-# Allow user to poweroff and reboot
-echo -e 'permit nopass :wheel cmd poweroff\npermit nopass :wheel cmd reboot' | sudo tee -a /etc/doas.conf
-
-case $PACKAGES in
-	minimal) ;;
-	server)
-		# Enabling the dotfiles
-		cd /home/$USERNAME/git/dotfiles
-		sudo -u $USERNAME bash auto.sh server
-		sudo bash auto.sh server
-	;;
-	virtual|laptop)
-		# Enabling the dotfiles
-		cd /home/$USERNAME/git/dotfiles
-		sudo -u $USERNAME bash auto.sh
-		sudo bash auto.sh
-
-		# Getting the wallpaper
-		mkdir /home/$USERNAME/Images
-		cd /home/$USERNAME/Images
-		wget -q --show-progress https://www.pixelstalk.net/wp-content/uploads/2016/07/HD-Astronaut-Wallpaper.jpg
-
-		# Allow user to use brightnessctl
-		test $PACKAGES == 'laptop' && echo 'permit nopass :wheel cmd brightnessctl' | sudo tee -a /etc/doas.conf
-	;;
-esac
-
-# Removing the nopass option in doas
-sudo sed -i '1s/nopass/persist/g' /etc/doas.conf
-
-" > /mnt/gentoo/home/$USERNAME/install.sh
-chmod +x /mnt/gentoo/home/$USERNAME/install.sh
-chroot --userspec $USERNAME:$USERNAME /mnt/gentoo /bin/bash /home/$USERNAME/install.sh
-
 # Cleaning leftovers
-rm -f /mnt/gentoo/install.sh /mnt/gentoo/$STAGE_FILENAME /mnt/gentoo/home/$USERNAME/install.sh
+rm -f /mnt/gentoo/install.sh /mnt/gentoo/$STAGE_FILENAME $0
 
 reboot
 
