a5fa42ed845165d2694d230fb6e06ae2b7e784b1
saundersp <pierre.saundersgb@gmail.com>
2022-04-06 21:39:42 +0200

    simplified auto.sh && added lfrc

--- auto.sh
+++ auto.sh
@@ -12,37 +12,31 @@ if [[ $1 == 'uninstall' || $1 == 'remove' ]]; then
 		$XDG_CONFIG_HOME/polybar/launch.sh $XDG_CONFIG_HOME/polybar/config $XDG_CONFIG_HOME/polybar/scripts $HOME/.bashrc $HOME/.bash_profile
 elif [ $EUID -ne 0 ]; then
 	test ! -r $XDG_CACHE_HOME/.neofetch && neofetch --config neofetch/config.conf > $XDG_CACHE_HOME/.neofetch
-	mkdir -p $XDG_CONFIG_HOME/neofetch $XDG_CONFIG_HOME/nvim/autoload/plugged
+	mkdir -p $XDG_CONFIG_HOME/neofetch $XDG_CONFIG_HOME/nvim/autoload/plugged $XDG_CONFIG_HOME/lf/lfrc
 
 	FILENAME=$XDG_CONFIG_HOME/nvim/autoload/plug.vim
 	test ! -f $FILENAME && curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > $FILENAME
 
-	rm -f $XDG_CONFIG_HOME/nvim/init.lua $XDG_CONFIG_HOME/neofetch/config.conf $HOME/.bashrc $HOME/.gitconfig $HOME/.bash_profile
-
-	ln -s $CURRENT_FOLDER/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
-	ln -s $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
-	ln -s $CURRENT_FOLDER/.bashrc $HOME/.bashrc
-	ln -s $CURRENT_FOLDER/.gitconfig $HOME/.gitconfig
+	ln -sf $CURRENT_FOLDER/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
+	ln -sf $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
+	ln -sf $CURRENT_FOLDER/lf/lfrc $XDG_CONFIG_HOME/lf/lfrc
+	ln -sf $CURRENT_FOLDER/.bashrc $HOME/.bashrc
+	ln -sf $CURRENT_FOLDER/.gitconfig $HOME/.gitconfig
 
 	if [[ $1 == 'server' ]]; then
-		ln -s $CURRENT_FOLDER/.bash_profile_server $HOME/.bash_profile
+		ln -sf $CURRENT_FOLDER/.bash_profile_server $HOME/.bash_profile
 	elif [[ $1 == 'windows' ]]; then
-		rm -f $XDG_CONFIG_HOME/alacritty/alacritty.yml $XDG_CONFIG_HOME/nvim/init.lua $XDG_CONFIG_HOME/neofetch/config.conf $HOME/.bashrc $HOME/.gitconfig \
-			$XDG_CONFIG_HOME/nvim/coc-settings.json
 
 		mkdir -p $XDG_CONFIG_HOME/alacritty
 
-		ln -s $CURRENT_FOLDER/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
-		ln -s $CURRENT_FOLDER/win.bash_profile $HOME/.bash_profile
-		ln -s $CURRENT_FOLDER/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
-		ln -s $CURRENT_FOLDER/nvim/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
-		ln -s $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
-		ln -s $CURRENT_FOLDER/.bashrc $HOME/.bashrc
-		ln -s $CURRENT_FOLDER/.gitconfig $HOME/.gitconfig
+		ln -sf $CURRENT_FOLDER/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
+		ln -sf $CURRENT_FOLDER/win.bash_profile $HOME/.bash_profile
+		ln -sf $CURRENT_FOLDER/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
+		ln -sf $CURRENT_FOLDER/nvim/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
+		ln -sf $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
+		ln -sf $CURRENT_FOLDER/.bashrc $HOME/.bashrc
+		ln -sf $CURRENT_FOLDER/.gitconfig $HOME/.gitconfig
 	else
-		rm -rf $XDG_CONFIG_HOME/alacritty/alacritty.yml $XDG_CONFIG_HOME/i3/config $HOME/.xinitrc $XDG_CONFIG_HOME/nvim/init.lua \
-			$XDG_CONFIG_HOME/polybar/launch.sh $XDG_CONFIG_HOME/polybar/config $XDG_CONFIG_HOME/polybar/scripts $XDG_CONFIG_HOME/nvim/coc-settings.json
-
 		mkdir -p $XDG_CONFIG_HOME/alacritty $XDG_CONFIG_HOME/i3 $XDG_CONFIG_HOME/i3blocks $XDG_CONFIG_HOME/polybar
 
 		FILENAME=$CURRENT_FOLDER/polybar/scripts/system-bluetooth-bluetoothctl.sh
@@ -58,21 +52,20 @@ elif [ $EUID -ne 0 ]; then
 			chmod +x $FILENAME
 		fi
 
-		ln -s $CURRENT_FOLDER/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
-		ln -s $CURRENT_FOLDER/polybar/launch.sh $XDG_CONFIG_HOME/polybar/launch.sh
-		ln -s $CURRENT_FOLDER/polybar/config $XDG_CONFIG_HOME/polybar/config
-		ln -s $CURRENT_FOLDER/polybar/scripts $XDG_CONFIG_HOME/polybar/scripts
-		ln -s $CURRENT_FOLDER/i3/config $XDG_CONFIG_HOME/i3/config
-		ln -s $CURRENT_FOLDER/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
-		ln -s $CURRENT_FOLDER/nvim/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
-		ln -s $CURRENT_FOLDER/.xinitrc $HOME/.xinitrc
-		ln -s $CURRENT_FOLDER/.bash_profile $HOME/.bash_profile
+		ln -sf $CURRENT_FOLDER/alacritty/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
+		ln -sf $CURRENT_FOLDER/polybar/launch.sh $XDG_CONFIG_HOME/polybar/launch.sh
+		ln -sf $CURRENT_FOLDER/polybar/config $XDG_CONFIG_HOME/polybar/config
+		ln -sf $CURRENT_FOLDER/polybar/scripts $XDG_CONFIG_HOME/polybar/scripts
+		ln -sf $CURRENT_FOLDER/i3/config $XDG_CONFIG_HOME/i3/config
+		ln -sf $CURRENT_FOLDER/nvim/init.lua $XDG_CONFIG_HOME/nvim/init.lua
+		ln -sf $CURRENT_FOLDER/nvim/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
+		ln -sf $CURRENT_FOLDER/.xinitrc $HOME/.xinitrc
+		ln -sf $CURRENT_FOLDER/.bash_profile $HOME/.bash_profile
 	fi
 else
 	mkdir -p $HOME/.XDG/cache $XDG_CONFIG_HOME/neofetch
-	rm -r $XDG_CONFIG_HOME/neofetch/config.conf $HOME/.bashrc $HOME/.bash_profile
 
-	ln -s $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
-	ln -s $CURRENT_FOLDER/.bashrc $HOME/.bashrc
-	ln -s $CURRENT_FOLDER/root.bash_profile $HOME/.bash_profile
+	ln -sf $CURRENT_FOLDER/neofetch/config.conf $XDG_CONFIG_HOME/neofetch/config.conf
+	ln -sf $CURRENT_FOLDER/.bashrc $HOME/.bashrc
+	ln -sf $CURRENT_FOLDER/root.bash_profile $HOME/.bash_profile
 fi
