3008d17295c9832ab1501a411d6a489d491a1701
saundersp <pierre.saundersgb@gmail.com>
2022-05-17 22:29:21 +0200

    patches : added font2 patches && merged all patches into one

--- patches/st.sh
+++ patches/st.sh
@@ -14,14 +14,7 @@ __apply_patch__(){
 		rm *.orig *.rej
 
 		cd -
-		patch -p1 -t -d $st_path < ./st-xresources-signal-reloading-20220407-ef05519.diff
-		patch -p0 -t -d $st_path < ./st-mousescroll.diff
-		patch -p1 -t -d $st_path < ./st-visualbell2-basic-2020-05-13-045a0fa.diff
-		patch -p0 -t -d $st_path < ./st-delkey-20201112-4ef0cbd.diff
-		patch -p1 -t -d $st_path < ./st-scrollback-0.8.5.diff
-		patch -p1 -t -d $st_path < ./st-ligatures-boxdraw-20210824-0.8.4.diff
-		patch -p1 -t -d $st_path < ./st-boxdraw_v2-0.8.5.diff
-		patch -p1 -t -d $st_path < ./st-hasklug-font.diff
+		patch -p1 -t -d $st_path < ./st-patches.diff
 
 		cd $st_path
 		rm *.orig *.rej config.h
@@ -30,14 +23,7 @@ __apply_patch__(){
 		exit 0
 	fi
 
-	patch -p1 -d $st_path < ./st-hasklug-font.diff
-	patch -p1 -d $st_path < ./st-boxdraw_v2-0.8.5.diff
-	patch -p1 -d $st_path < ./st-ligatures-boxdraw-20210824-0.8.4.diff
-	patch -p1 -d $st_path < ./st-scrollback-0.8.5.diff
-	patch -p0 -d $st_path < ./st-delkey-20201112-4ef0cbd.diff
-	patch -p1 -d $st_path < ./st-visualbell2-basic-2020-05-13-045a0fa.diff
-	patch -p0 -d $st_path < ./st-mousescroll.diff
-	patch -p1 -d $st_path < ./st-xresources-signal-reloading-20220407-ef05519.diff
+	patch -p1 -d $st_path < ./st-patches.diff
 	cd $st_path
 	make install
 	cd -
