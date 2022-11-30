6fc0923bfdb40c5c1457876da50109dfc3a9535b
saundersp <pierre.saundersgb@gmail.com>
2022-11-30 10:46:38 +0100

    patches : unified dmenu.sh & st.sh to a single patch.sh

---
+++ patches/patch.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Debug changes to repository
+# git am patch_file.diff
+# Update the patch file (LENGTH = dmenu:7 st:10)
+# git format-patch --stdout HEAD~$LENGTH > patch_file.diff
+
+__apply_patch__(){
+	if [ "$(id -u)" -ne 0 ]; then
+		echo Root priviliges required !
+		exit 1
+	fi
+	if [ -z "$1" ]; then
+		echo You have to enter a program name to patch !
+		exit 1
+	fi
+	local prog=$1
+	shift
+
+	local prog_path=/usr/local/src/$prog
+	local patches_dir=$(pwd)
+
+	if [ ! -f ./$prog-patches.diff ]; then
+		echo Missing patch file !
+		exit 1
+	fi
+
+	if [ "$1" = 'clean' ]; then
+		cd $prog_path
+		make uninstall
+		make clean
+		rm *.orig *.rej
+
+		cd -
+		patch -p1 -R -t -d $prog_path < ./$prog-patches.diff
+
+		cd $prog_path
+		rm *.orig *.rej config.h
+		git checkout *
+		cd -
+		exit 0
+	fi
+
+	patch -p1 -d $prog_path < ./$prog-patches.diff
+	cd $prog_path
+	make install
+	cd -
+}
+__apply_patch__ $*
+unset __apply_patch__
