fffbd9f5f150c07032acea76f7f944964559d3da
saundersp <pierre.saundersgb@gmail.com>
2024-02-20 21:27:11 +0100

    installer/gentoo.sh : updated to current usage & massively improved flow

--- installer/gentoo.sh
+++ installer/gentoo.sh
@@ -81,15 +81,12 @@ swapon /mnt/gentoo/swap
 cd /mnt/gentoo
 
 # links gentoo.org/downloads
-STAGE_NAME=$(curl -s http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-$STAGE_TYPE.txt | grep -v '^#' | cut -d' ' -f 1)
-wget http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE_NAME
-STAGE_FILENAME=$(basename $STAGE_NAME)
-tar xpf $STAGE_FILENAME --xattrs-include='*.*' --numeric-owner
+STAGE_NAME="$(curl -s http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-$STAGE_TYPE.txt | head -n 6 | tail -n 1 | cut -d' ' -f 1)"
+wget http://distfiles.gentoo.org/releases/amd64/autobuilds/"$STAGE_NAME"
+STAGE_FILENAME=$(basename "$STAGE_NAME")
+tar xpf "$STAGE_FILENAME" --xattrs-include='*.*' --numeric-owner
 
-sed -i 's/COMMON_FLAGS="/COMMON_FLAGS="-march=native /g' /mnt/gentoo/etc/portage/make.conf
-echo -e "MAKEOPTS=\"-j$(nproc)\"\nACCEPT_LICENSE=\"*\"\nACCEPT_KEYWORDS=\"~amd64\"" >> /mnt/gentoo/etc/portage/make.conf
-
-mirrorselect -a -s 20 -o >> /mnt/gentoo/etc/portage/make.conf
+mirrorselect -s 20 -o >> /mnt/gentoo/etc/portage/make.conf
 
 mkdir -p /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
@@ -120,38 +117,81 @@ emerge -q --sync
 # Choosing the default minimal profile
 eselect profile set 1
 
-# Apply the default USE flags to the @world set
-emerge -q --update --deep --newuse @world
-
 # Removing the existing package.use
 rm -rf /etc/portage/package.use /etc/portage/package.accept_keywords /etc/portage/package.mask
 touch /etc/portage/package.use /etc/portage/package.accept_keywords /etc/portage/package.mask
 
 # Configuring the USE flags
 # Get all enabled flags : portageq envvar USE | xargs -n1
-# Note : use 'less /var/db/repos/gentoo/profiles/use.desc' to get a description of each variables
 case $PACKAGES in
 	minimal)
 		# Global flags
-		echo 'USE=\"elogind\"' >> /etc/portage/make.conf
+		echo 'USE=\"minimal elogind -filecaps -caps\"' >> /etc/portage/make.conf
+		echo -e 'sys-boot/grub mount -themes -fonts\napp-alternatives/sh -bash dash' > /etc/portage/package.use
 	;;
 	server)
 		# Global flags
-		echo 'USE=\"X bash-completion elogind python\"' >> /etc/portage/make.conf
+		echo 'USE=\"bash-completion minimal elogind -filecaps -caps\"' >> /etc/portage/make.conf
+		echo -e 'sys-boot/grub mount -themes -fonts\napp-alternatives/sh -bash dash' > /etc/portage/package.use
 	;;
 	virtual)
 		# Global flags
-		echo 'USE=\"X bash-completion elogind python\"' >> /etc/portage/make.conf
+		echo 'USE=\"X bash-completion minimal elogind network threads -wifi -filecaps -caps\"' >> /etc/portage/make.conf
 		# Local flags
-		echo -e 'x11-misc/polybar i3wm\nmedia-libs/libvpx postproc' > /etc/portage/package.use
+		echo -e 'x11-misc/picom opengl -pcre -drm\nmedia-gfx/imagemagick jpeg\nx11-misc/polybar i3wm ipc network doc\nsys-boot/grub mount -themes -fonts
+x11-base/xorg-server -minimal\napp-alternatives/sh -bash dash\napp-emulation/virtualbox-guest-additions gui
+
+# required by app-portage/pfl-3.2-r2::gentoo[network-cron]
+>=sys-apps/util-linux-2.38.1-r2 caps
+# required by dev-lang/zig-0.10.1-r2::gentoo
+# required by sys-fs/ncdu-2.2.2-r1::gentoo
+>=sys-devel/lld-15.0.7 zstd
+>=sys-devel/llvm-15.0.7 zstd
+
+# required by x11-drivers/xf86-video-vmware-13.4.0::gentoo
+# required by x11-base/xorg-drivers-21.1-r2::gentoo[video_cards_vmware]
+# required by x11-base/xorg-server-21.1.7::gentoo[xorg]
+# required by x11-drivers/xf86-input-libinput-1.2.1::gentoo
+>=media-libs/mesa-23.0.0-r1 xa
+# required by media-sound/pulseaudio-daemon-16.1-r6::gentoo[alsa-plugin,alsa]
+# required by media-libs/libpulse-16.1-r2::gentoo
+# required by media-sound/pulseaudio-16.1::gentoo
+# required by media-sound/pulsemixer-1.5.1-r1::gentoo
+>=media-plugins/alsa-plugins-1.2.7.1-r1 pulseaudio
+
+# required by www-client/librewolf-110.0_p2::librewolf[system-libvpx]
+>=media-libs/libvpx-1.12.0-r1 postproc
+' > /etc/portage/package.use
+		echo 'VIDEO_CARDS=\"vmware\"' >> /etc/portage/make.conf
 	;;
 	laptop)
 		# Global flags
-		echo 'USE=\"X bash-completion connman elogind pulseaudio python\"' >> /etc/portage/make.conf
+		echo 'USE=\"X bash-completion minimal elogind pulseaudio network threads wifi -filecaps -caps\"' >> /etc/portage/make.conf
 		# Local flags
-		echo -e 'x11-misc/polybar i3wm\nmedia-libs/libvpx postproc' > /etc/portage/package.use
+		echo -e 'x11-misc/picom opengl -pcre -drm\nmedia-gfx/imagemagick jpeg\nx11-misc/polybar i3wm ipc network doc\nsys-boot/grub mount -themes -fonts
+x11-base/xorg-server -minimal\napp-alternatives/sh -bash dash'> /etc/portage/package.use
+		echo 'VIDEO_CARDS=\"intel nvidia\"' >> /etc/portage/make.conf
 	;;
 esac
+
+sed -i 's/COMMON_FLAGS=\\\"/COMMON_FLAGS=\\\"-march=native /g' /etc/portage/make.conf
+echo -e \"MAKEOPTS=\\\"-j\$(nproc) -l\$(nproc)\\\"\nEMERGE_DEFAULT_OPTS=\\\"-j\$(nproc) -l\$(nproc)\\\"\nACCEPT_LICENSE=\\\"*\\\"\nACCEPT_KEYWORDS=\\\"~amd64\\\"\" >> /etc/portage/make.conf
+
+emerge -q --noreplace app-misc/resolve-march-native
+
+sed -i \"s/COMMON_FLAGS=\\\"-march=native /COMMON_FLAGS=\\\"\$(resolve-march-native) /g\" /etc/portage/make.conf
+echo 'FEATURES=\"parallel-install parallel-fetch\"' >> /etc/portage/make.conf
+
+# Apply the USE flags to the @world set
+emerge -q --update --deep --newuse @world
+
+# Removing outdated packages
+emerge -cD
+
+# Updating profile if neccessary
+eselect gcc set 1
+eselect binutils set 1
+source /etc/profile
 
 # Setting the timezone
 echo $TIMEZONE > /etc/timezone
@@ -165,31 +205,30 @@ env-update
 source /etc/profile
 
 # Installing cryptsetup
-emerge -q sys-fs/cryptsetup
+emerge -q --noreplace sys-fs/cryptsetup
 rc-update add dmcrypt boot
 
 # Compilation and installation of the kernel
-emerge -q sys-kernel/gentoo-sources
+emerge -q --noreplace sys-kernel/gentoo-sources
 eselect kernel set 1
-#cd /usr/src/linux
-#make menuconfig
-#make -j\$(nproc)
-#make modules_install -j\$(nproc)
-#make install
-#emerge -q sys-apps/pciutils
+cd /usr/src/linux
+wget https://raw.githubusercontent.com/saundersp/dotfiles/main/installer/${PACKAGES^^}.config -O .config
+make olddefconfig
+make -j\$(nproc) -l\$(nproc)
+make modules_install -j\$(nproc)
+make install
 
 # Building the kernel and initramfs
-emerge -q sys-kernel/genkernel
-genkernel --luks all
-#genkernel --luks initramfs
+emerge -q --noreplace sys-kernel/genkernel
+genkernel --luks initramfs
 
 # Installing the firmware
-emerge -q sys-kernel/linux-firmware
+emerge -q --noreplace sys-kernel/linux-firmware
 
 # Setting up the fstab auto mouting
 echo \"
 UUID=\$(blkid -s UUID -o value $BOOT_PARTITION) /boot vfat defaults,noatime 0 2
-UUID=\$(blkid -s UUID -o value /dev/mapper/$CRYPTED_DISK_NAME) / ext4 noatime 0 1
+UUID=\$(blkid -s UUID -o value /dev/mapper/$CRYPTED_DISK_NAME) / ext4 defaults,noatime 0 1
 /swap none swap defaults 0 0
 \" >> /etc/fstab
 
@@ -202,8 +241,7 @@ cat << EOF
 EOF > /etc/hosts
 
 # Setting network
-emerge -q --noreplace net-misc/netifrc
-emerge -q net-misc/dhcpcd
+emerge -q --noreplace net-misc/netifrc net-misc/dhcpcd
 rc-update add dhcpcd default
 
 # Setting a root password
@@ -222,7 +260,7 @@ $USER_PASSWORD
 EOF
 
 # Installing and setting up doas
-emerge -q app-admin/doas
+emerge -q --noreplace app-admin/doas
 echo -e 'permit nopass :wheel\npermit nopass :wheel cmd poweroff\npermit nopass :wheel cmd reboot' > /etc/doas.conf
 
 # Replace sudo
@@ -232,15 +270,15 @@ ln -s /usr/bin/doas /usr/bin/sudo
 sed -i 's/\"us\"/\"$KEYMAP\"/g' /etc/conf.d/keymaps
 
 # Setup logging system
-emerge -q app-admin/sysklogd
+emerge -q --noreplace app-admin/sysklogd
 rc-update add sysklogd default
 
 # Getting the filesystems packages
-emerge -q sys-fs/e2fsprogs sys-fs/dosfstools
+emerge -q --noreplace sys-fs/e2fsprogs sys-fs/dosfstools
 
 # Installing the bootloader
 echo 'GRUB_PLATFORMS=\"efi-64\"' >> /etc/portage/make.conf
-emerge -q sys-boot/grub
+emerge -q --noreplace sys-boot/grub
 
 # Prepare boot loader for LUKS
 sed -i \"s,^#GRUB_CMDLINE_LINUX=\\\"\\\",GRUB_CMDLINE_LINUX=\\\"quiet crypt_root=UUID=\$(blkid -s UUID -o value $ROOT_PARTITION) root=/dev/mapper/root keymap=$KEYMAP\\\",g\" /etc/default/grub
@@ -248,60 +286,63 @@ sed -i \"s,^#GRUB_CMDLINE_LINUX=\\\"\\\",GRUB_CMDLINE_LINUX=\\\"quiet crypt_root
 # Enable os-prober if laptop
 if [ '$PACKAGES' == 'laptop' ]; then
 	echo GRUB_DISABLE_OS_PROBER=false >> /etc/default/grub
-	emerge -q sys-boot/os-prober
+	emerge -q --noreplace sys-boot/os-prober
 	os-prober
 fi
 
 # Installing GRUB bootloader
-grub-install --target x86_64-efi --efi-directory /boot --removable --bootloader-id $GRUB --recheck
+grub-install --target x86_64-efi --efi-directory /boot --removable --bootloader-id $GRUB_ID --recheck
 
 # Creating the GRUB configuration file
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Installing minimal packages
-emerge -q sys-auth/elogind sys-apps/which
+emerge -q --noreplace sys-auth/elogind app-eselect/eselect-python
+
+# Change default editor
+eselect pager set less
 
 install_server(){
-	emerge -q app-misc/neofetch app-editors/neovim sys-apps/which dev-vcs/git net-misc/wget app-arch/unzip app-shells/bash-completion net-libs/nodejs \
-			dev-lang/python sys-apps/ripgrep sys-apps/man-db dev-python/pip sys-process/htop dev-lang/go app-misc/tmux app-misc/ranger
+	emerge -q --noreplace app-eselect/eselect-repository dev-vcs/git
+	eselect repository enable guru
+	emaint -r guru sync
+
+	eselect repository add saundersp-overlay git https://git.saundersp.com/saundersp/saundersp-overlay.git
+	emaint -r saundersp-overlay sync
+
+	emerge -q --noreplace app-misc/fastfetch app-editors/neovim sys-apps/which net-misc/wget app-arch/unzip app-shells/bash-completion \
+		net-libs/nodejs dev-lang/python sys-apps/ripgrep sys-apps/man-db dev-python/pip sys-process/btop dev-lang/go app-misc/tmux \
+		app-misc/ranger dev-python/pynvim app-portage/mirrorselect app-portage/pfl app-portage/gentoolkit sys-apps/fd sys-fs/ncdu \
+		app-portage/eix dev-vcs/lazygit app-containers/lazydocker
+
+	# Update eix packages cache
+	eix-update
+	eix-remote update
+
+	# Change default editor to neovim
+	eselect vi set nvim
+	eselect editor set vi
+	eselect visual set vi
 
 	# Creating user's custom packages location
 	mkdir /usr/local/src
 
-	# Compiling lazygit
-	cd /usr/local/src
-	git clone https://github.com/jesseduffield/lazygit.git
-	cd lazygit
-	sudo -u $USERNAME go install
-	mv /home/$USERNAME/go/bin/lazygit /usr/bin/lazygit
-
-	# Compiling lazygit
-	cd ..
-	git clone https://github.com/jesseduffield/lazydocker.git
-	cd lazydocker
-	sudo -u $USERNAME go install
-	mv /home/$USERNAME/go/bin/lazydocker /usr/bin/
-	cd \$HOME
-
 	# Installing npm dependencies
 	npm i -g neovim npm-check-updates
-
-	# Installing pip dependencies
-	sudo -u $USERNAME pip install --user pynvim autopep8 flake8
-	ls /home/$USERNAME/.local/bin | xargs -I{} sudo mv /home/$USERNAME/.local/bin/{} /usr/bin/{}
 }
 install_ihm(){
 	install_server
-	emerge -q x11-misc/dmenu x11-misc/picom x11-wm/i3-gaps x11-apps/xinit x11-base/xorg-server x11-apps/xset media-gfx/feh \
-			x11-misc/polybar x11-apps/xrandr x11-misc/xclip x11-apps/setxkbmap app-eselect/eselect-repository \
-			x11-libs/libXinerama media-gfx/ueberzug media-gfx/imagemagick
+	emerge -q --noreplace x11-misc/picom x11-wm/i3 x11-apps/xinit x11-base/xorg-server x11-apps/xset media-gfx/feh \
+		x11-misc/polybar x11-apps/xrandr x11-misc/xclip x11-apps/setxkbmap app-eselect/eselect-repository \
+		x11-libs/libXinerama media-gfx/ueberzugpp media-gfx/imagemagick media-sound/pulsemixer
 
-	eselect repository add librewolf git https://gitlab.com/librewolf-community/browser/gentoo.git
+	eselect repository add librewolf git https://codeberg.org/librewolf/gentoo.git
 	emaint -r librewolf sync
-	emerge -q www-client/librewolf
+	#emerge -q --noreplace www-client/librewolf
 
 	# Getting the Hasklig font
-	wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hasklig.zip
+	LATEST_TAG=\$(curl https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep tag_name | cut -d \\\" -f 4)
+	wget -q --show-progress https://github.com/ryanoasis/nerd-fonts/releases/download/\"\$LATEST_TAG\"/Hasklig.zip
 	mkdir /usr/share/fonts/Hasklig
 	unzip -q Hasklig.zip -d /usr/share/fonts/Hasklig
 	rm Hasklig.zip
@@ -330,7 +371,9 @@ export -f install_dotfiles
 case $PACKAGES in
 	virtual)
 		install_ihm
-		emerge -q app-emulation/virtualbox-guest-additions
+		emerge -q --noreplace app-emulation/virtualbox-guest-additions
+		rc-update add virtualbox-guest-additions default
+		usermod -aG vboxguest $USERNAME
 		su $USERNAME -c \"install_dotfiles $PACKAGES\"
 	;;
 	server)
@@ -340,7 +383,7 @@ case $PACKAGES in
 	minimal) ;;
 	laptop)
 		install_ihm
-		emerge -q x11-apps/xbacklight net-misc/connman net-wireless/wpa_supplicant net-vpn/wireguard-tools
+		emerge -q --noreplace x11-apps/xbacklight net-misc/connman net-wireless/wpa_supplicant net-vpn/wireguard-tools
 		su $USERNAME -c \"install_dotfiles $PACKAGES\"
 	;;
 esac
