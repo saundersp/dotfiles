8a31474bf3a009d25ee32606ec58418a7fa0848d
saundersp <pierre.saundersgb@gmail.com>
2024-04-30 00:26:41 +0200

    Added dooit configuration

--- auto.sh
+++ auto.sh
@@ -25,14 +25,15 @@ if [ "$1" = '-r' ] || [ "$1" = 'r' ] || [ "$1" = 'remove' ] || [ "$1" = '--remov
 	rm -rf "$XDG_CONFIG_HOME"/nvim/init.lua "$XDG_CONFIG_HOME"/fastfetch/config.conf "$HOME"/.bashrc "$XDG_CONFIG_HOME"/git/config "$HOME"/.profile \
 		"$XDG_CONFIG_HOME"/i3/config "$HOME"/.xinitrc "$XDG_CONFIG_HOME"/nvim/coc-settings.json "$XDG_CONFIG_HOME"/X11/Xresources \
 		"$XDG_CONFIG_HOME"/polybar/launch.sh "$XDG_CONFIG_HOME"/polybar/config.ini "$XDG_CONFIG_HOME"/polybar/scripts \
-		"$XDG_CONFIG_HOME"/ranger/rc.conf "$XDG_CONFIG_HOME"/ranger/plugins "$XDG_CONFIG_HOME"/tmux/tmux.conf
+		"$XDG_CONFIG_HOME"/ranger/rc.conf "$XDG_CONFIG_HOME"/ranger/plugins "$XDG_CONFIG_HOME"/tmux/tmux.conf "$XDG_CONFIG_HOME"/dooit/config.py
 
 	FILENAME="$XDG_DATA_HOME"/nvim/lazy/lazy.nvim
 	test -d "$FILENAME" && rm -rf "$FILENAME"
 	FILENAME="$XDG_CONFIG_HOME"/ranger/plugins/ranger_devicons
 	test -d "$FILENAME" && rm -rf "$FILENAME"
 elif [ "$(id -u)" -ne 0 ]; then
-	mkdir -p "$XDG_CONFIG_HOME"/nvim/autoload "$XDG_CONFIG_HOME"/ranger/plugins "$XDG_CONFIG_HOME"/tmux "$XDG_CONFIG_HOME"/git "$XDG_CONFIG_HOME"/fastfetch
+	mkdir -p "$XDG_CONFIG_HOME"/nvim/autoload "$XDG_CONFIG_HOME"/ranger/plugins "$XDG_CONFIG_HOME"/tmux "$XDG_CONFIG_HOME"/git "$XDG_CONFIG_HOME"/fastfetch \
+		"$XDG_CONFIG_HOME"/dooit
 
 	FILENAME="$XDG_DATA_HOME"/nvim/lazy/lazy.nvim
 	test ! -d "$FILENAME" && git clone --filter blob:none https://github.com/folke/lazy.nvim.git --branch stable "$FILENAME"
@@ -46,6 +47,7 @@ elif [ "$(id -u)" -ne 0 ]; then
 	ln -sf "$CURRENT_FOLDER"/bash/bashrc "$HOME"/.bashrc
 	ln -sf "$CURRENT_FOLDER"/git/config "$XDG_CONFIG_HOME"/git/config
 	ln -sf "$CURRENT_FOLDER"/tmux/tmux.conf "$XDG_CONFIG_HOME"/tmux/tmux.conf
+	ln -sf "$CURRENT_FOLDER"/dooit/config.py "$XDG_CONFIG_HOME"/dooit/config.py
 
 	if [ "$1" = '-s' ] || [ "$1" = 's' ] || [ "$1" = 'server' ] || [ "$1" = '--server' ]; then
 		ln -sf "$CURRENT_FOLDER"/shell_profile/profile_server "$HOME"/.profile
