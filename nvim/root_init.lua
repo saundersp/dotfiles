722cb91ff830f608d97a8c74620b52fb38ca8ecd
saundersp <pierre.saundersgb@gmail.com>
2024-05-21 21:52:27 +0200

    nvim/root_init.lua : updated to be more alike with user one

--- nvim/root_init.lua
+++ nvim/root_init.lua
@@ -1,71 +1,76 @@
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Global shortcuts/helper
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-local function map(mode, key, action, desc, buffer)
+local function map(mode, key, action, desc)
 	if type(key) == 'table' then
-		for _,e in ipairs(key) do
-			map(mode, e, action, desc, buffer)
+		for _, e in ipairs(key) do
+			map(mode, e, action, desc)
 		end
 	else
-		vim.keymap.set(mode, key, action, { silent = true, buffer = buffer, desc = desc })
+		vim.keymap.set(mode, key, action, { silent = true, desc = desc })
 	end
 end
-local function nmap(key, action, desc, buffer) map('n', key, action, desc, buffer) end
-local function vmap(key, action, desc, buffer) map('v', key, action, desc, buffer) end
-local function Autocmd(events, pattern, callback)
-	vim.api.nvim_create_autocmd(events, { pattern = pattern, callback = callback })
+local function nmap(key, action, desc) map('n', key, action, desc) end
+local function vmap(key, action, desc) map('v', key, action, desc) end
+
+local function Autocmd(events, pattern, callback, desc)
+	vim.api.nvim_create_autocmd(events, { pattern = pattern, callback = callback, desc = desc })
+end
+
+local function create_cmd(cmd, fnc, desc, nargs)
+	vim.api.nvim_create_user_command(cmd, fnc, { nargs = nargs or 0, desc = desc })
 end
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- General settings configuration
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 vim.cmd.colorscheme				('desert')									-- Set the colour scheme to a more readable one
-vim.o.syntax					= true										-- Enables syntax highlighting
 vim.o.listchars					= 'eol:󰌑,tab:󰌒 ,trail:•,extends:,precedes:,space:·,nbsp:󱁐'			-- List of whitespace characters replacement (see :h listchars) (using: nf-md-keyboard_return nf-md-keyboard_tab Bullet nf-cod-chevron_right nf-cod-chevron_left Interpunct nf-md-keyboard_space)
-vim.o.list					= true										-- Enable replacement of listchars
-vim.o.wrap					= false										-- Display long lines as just one line
-vim.o.fileencoding				= 'UTF-8'									-- The encoding written to file
-vim.o.iskeyword					= vim.o.iskeyword .. ',-'							-- treat dash separated words as a word text object
-vim.o.tabstop					= 8										-- Set the width of a tab
-vim.o.shiftwidth				= 8										-- Change the number of space characters inserted for indentation
-vim.o.softtabstop				= 8										-- Change the number of space characters inserted for indentation
-vim.o.smartindent				= true										-- Does smart autoindenting when starting a new line
-vim.o.number					= true										-- Line numbers
-vim.o.relativenumber				= true										-- Relative number (enabled after number for hybrid mode)
-vim.o.cursorline				= true										-- Enable highlighting of the current line
-vim.o.cursorcolumn				= true										-- Enable highlighting of the current column
+vim.wo.list					= true										-- Enable replacement of listchars
+vim.wo.wrap					= false										-- Display long lines as just one line
+vim.bo.fileencoding				= 'UTF-8'									-- The encoding written to file
+vim.bo.iskeyword				= vim.bo.iskeyword .. ',-'							-- treat dash separated words as a word text object
+vim.bo.tabstop					= 8										-- Set the width of a tab
+vim.bo.shiftwidth				= 8										-- Change the number of space characters inserted for indentation
+vim.bo.softtabstop				= 8										-- Change the number of space characters inserted for indentation
+vim.bo.smartindent				= true										-- Does smart autoindenting when starting a new line
+vim.wo.number					= true										-- Line numbers
+vim.wo.relativenumber				= true										-- Relative number (enabled after number for hybrid mode)
+vim.wo.cursorline				= true										-- Enable highlighting of the current line
+vim.wo.cursorcolumn				= true										-- Enable highlighting of the current column
 vim.o.showtabline				= 2										-- Always show top files tabs
 vim.o.showmode					= false										-- We don't need to see things like -- INSERT -- any more
-vim.o.foldlevel					= 99										-- Fold are open when you first open a file
+vim.wo.foldlevel				= 99										-- Fold are open when you first open a file
 vim.o.visualbell				= true										-- Disable bell noise
 vim.o.splitbelow				= true										-- Horizontal splits will automatically be below
 vim.o.splitright				= true										-- Vertical splits will automatically be to the right
 vim.o.completeopt				= 'menuone,noselect'								-- Add LSP complete popup menu
-vim.o.signcolumn				= 'yes'										-- Always draw the signcolumn with 1 fixed space width
+vim.wo.signcolumn				= 'yes:1'									-- Draw the signcolumn with 1 fixed space width
 vim.o.title					= true										-- Change the window's title to the opened file name and directory
 vim.o.updatetime				= 200										-- Time before CursorHold triggers
-vim.o.swapfile					= false										-- Disable swapfile usage
+vim.bo.swapfile					= false										-- Disable swapfile usage
 vim.o.wildmode					= 'longest,list,full'								-- Enable autocompletion in COMMAND mode
-vim.o.formatoptions				= vim.o.formatoptions .. 'r'							-- Add asterisks in block comments
+vim.bo.formatoptions				= vim.o.formatoptions .. 'r'							-- Add asterisks in block comments
 vim.o.wildignore				= '*.o,*.obj,*/node_modules/*,*/.git/*,*/venv/*,*/package-lock.json'		-- Ignore files in fuzzy finder
-vim.o.undofile					= true										-- Enable undofile to save undos after exit
+vim.bo.undofile					= true										-- Enable undofile to save undos after exit
 vim.o.scrolloff					= 8										-- Minimal number of screen lines to keep above and below the cursor.
-Autocmd('Filetype', 'tex',			function() vim.o.wrap = true end)						-- Enable wraping only for LaTeX files
-Autocmd('Filetype', 'python',			function() vim.o.expandtab = false end)						-- Disable the tab expansion of spaces
-Autocmd({ 'BufRead', 'BufNewFile' }, '*.tex',	function() vim.o.filetype = 'tex' end)						-- Sometimes LaTeX isn't properly recognized
+Autocmd('Filetype', 'tex',			function() vim.o.wrap = true end,						   'Enable wraping only for LaTeX files')
+Autocmd('Filetype', 'python',			function() vim.o.expandtab = false end,						   'Disable the tab expansion of spaces')
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Key mapping configuration
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 nmap('<C-s>', '<cmd>w<CR>',													   'Save buffer shortcut')
 nmap({ '<C-F4>', '<F28>' }, '<cmd>tabclose!<CR>',										   'Close tab shortcut (keeps buffer open)')
+nmap('<C-S-h>', '<cmd>-tabmove<CR>',												   'Move the current tab to the left')
+nmap('<C-S-l>', '<cmd>+tabmove<CR>',												   'Move the current tab to the right')
 nmap({ '<S-F4>', '<F16>' }, '<cmd>bd<CR>',											   'Close buffer shortcut')
 nmap('<M-j>', '<cmd>resize -1<CR>',												   'Decrease buffer window horizontal size (M is the ALT modifier key)')
 nmap('<M-k>', '<cmd>resize +1<CR>',												   'Increase buffer window horizontal size (M is the ALT modifier key)')
 nmap('<M-h>', '<cmd>vertical resize -1<CR>',											   'Decrease buffer window vertical size (M is the ALT modifier key)')
 nmap('<M-l>', '<cmd>vertical resize +1<CR>',											   'Increase buffer window vertical size (M is the ALT modifier key)')
 map('t', '<Esc>', '<C-\\><C-n>',												   'Fix terminal exit button')
-nmap({'<C-m>', '<CR>'}, '<cmd>noh<CR>',												   'Clear the highlighting of :set hlsearch (<C-M> == <CR> in st)')
+nmap({ '<C-m>', '<CR>' }, '<cmd>noh<CR>',											   'Clear the highlighting of :set hlsearch (<C-M> == <CR> in st)')
 nmap('<C-z>', '<Nop>',														   'Disable the suspend signal')
 vmap('<', '<gv',														   'Shift the selection one indent to the right')
 vmap('>', '>gv',														   'Shift the selection one indent to the left')
@@ -82,4 +87,4 @@ nmap('<C-d>', '<C-d>zz',													   'Scroll window downwards in the buffer w
 nmap('<C-f>', '<C-f>zz',													   'Scroll window downwards in the buffer while keeping cursor at the middle of the window')
 nmap('<leader>fx', '<cmd>!chmod +x %<CR>',											   'Make the current file executable')
 nmap('<leader>fX', '<cmd>!chmod -x %<CR>',											   'Make the current file non executable')
-vim.api.nvim_create_user_command('EditConfig', 'e $MYVIMRC', { desc =								   'Edit Neovim config file' })
+create_cmd('EditConfig', 'e $MYVIMRC', 												   'Edit Neovim config file')
