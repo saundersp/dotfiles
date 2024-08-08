aa6cc689288ba3c739e6482c9d97b0127d9eccc4
saundersp <pierre.saundersgb@gmail.com>
2024-08-08 22:36:02 +0200

    polybar : now adds a bar for each connected monitor using xrandr

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# Terminate already running bar instances
+# Terminate already running bars instances
 pkill polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar --config="$XDG_CONFIG_HOME"/polybar/config.ini mybar 2>&1 | tee -a /tmp/polybar.log &
-
-echo "Polybar launched..."
+if command -v xrandr >> /dev/null; then
+	for monitor in $(xrandr --query | grep ' connected' | cut -d ' ' -f1); do
+		MONITOR="$monitor" polybar --reload my_bar 2>&1 | tee -a /tmp/polybar_"$monitor".log &
+		echo "Polybar launched on monitor $monitor"
+	done
+else
+	# Launch Polybar, using default config location ~/.config/polybar/config
+	polybar --reload my_bar 2>&1 | tee -a /tmp/polybar.log &
+	echo 'Polybar launched...'
+fi
